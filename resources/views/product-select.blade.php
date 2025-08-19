ea7281c8c91fc542f053a5284c945b7f639de110
ro749 <45647699+ro749@users.noreply.github.com>
2025-08-19 16:06:34 -0600

    Update product-select.blade.php

--- resources/views/product-select.blade.php
+++ resources/views/product-select.blade.php
@@ -13,14 +13,21 @@
 <body >
     @include('header')
     <div class="content" style="padding: 36px;">
-        
-        @include('shared-utils::components.tables.layeredSmartTable', ['table' => $table])
-        
+        <button class="btn btn-primary" onclick="open_select_product();">
+            Añadir Producto
+        </button>
+        <x-sharedutils::modal innerStyle="width: 90%; height: 90%;" id="select-product-popup" onclose="closePopup('select-product-popup');">
+            @include('shared-utils::components.tables.layeredSmartTable', ['table' => $table])
+        </x-sharedutils::modal>
     </div>
     @push('scripts')
     <script>
+        function open_select_product() {
+            openPopup('select-product-popup');
+            $('#{{ $table->id }}').refreshLayeredSmartTable();
+        }
         $(document).on('selected-ProductSelect', function(e, data) {
-            console.log(data);
+            
         });
     </script>
     @endpush
